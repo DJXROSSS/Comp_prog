c89dac44fef3f46cd5e160aab98e35ed34a4572c
vatsal80085 <vatsal08agarwal@gmail.com>
2025-08-07 14:43:38 +0530

    new fn added

--- calculator.java
+++ calculator.java
@@ -9,6 +9,10 @@ public class calculator {
         return a * b;
     }
 
+    public int subtract(int a, int b){
+        return a-b;
+    }
+
     public static void main(String[] args) {
         calculator calc = new calculator();
         int result = calc.plus(5, 3);
