f2ccc44979a60ec0df11fec15dbcf7e91db12953
AmanShakya0018 <amanshakya1808@gmail.com>
2025-08-07 14:40:12 +0530

    calc

---
+++ calculator.java
@@ -0,0 +1,13 @@
+
+public class calculator {
+
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        calculator calc = new calculator();
+        int result = calc.plus(5, 3);
+        System.out.println("5 + 3 = " + result);
+    }
+}
